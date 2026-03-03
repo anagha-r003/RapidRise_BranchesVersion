8b8d8db6e7a69063661b9a223605933771e6d724
Anagha R nair <anagharameshannair@gmail.com>
2026-03-03 09:45:32 +0530

    Initial structure for Task 2

---
+++ task2-jwt-crud-api/src/main/java/com/rapidrise/task2_jwt_crud_api/Task2JwtCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.rapidrise.task2_jwt_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2JwtCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2JwtCrudApiApplication.class, args);
+	}
+
+}
---
+++ task2-jwt-crud-api/src/test/java/com/rapidrise/task2_jwt_crud_api/Task2JwtCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidrise.task2_jwt_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task2JwtCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
